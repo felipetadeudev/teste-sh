550f922f1e59d867c5acc320881148ec05bdaaf5
Felipe Tadeu <felipetadeu.dev@gmail.com>
2024-09-28 20:17:06 +0000

    calma

--- wait-for-postgres.sh
+++ wait-for-postgres.sh
@@ -6,9 +6,17 @@ host="$1"
 shift
 cmd="$@"
 
+timeout=60 # Tempo limite em segundos
+count=0
+
 until PGPASSWORD="Extreme123" psql -h "$host" -U "postgres" -c '\q'; do
   >&2 echo "PostgreSQL is unavailable - sleeping"
   sleep 1
+  count=$((count + 1))
+  if [ $count -gt $timeout ]; then
+    >&2 echo "Error: PostgreSQL did not start within the timeout."
+    exit 1
+  fi
 done
 
 >&2 echo "PostgreSQL is up - executing command"
